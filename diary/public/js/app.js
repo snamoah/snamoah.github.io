2c46ba06bf4bafbb79e29660cdeeb8fb9e55f67c
Samuel Amoah <snamoah@gmail.com>
2014-10-10 16:02:17 +0000

    Connection to server 2

--- diary/public/js/app.js
+++ diary/public/js/app.js
@@ -1,5 +1,6 @@
 angular.module('notebookApp', [])
-	.controller('mainController', ['$scope', '$http', '$templateCache' function($scope, $http, $templateCache) {
+	.controller('mainController', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
+		$scope.names = "This is me";
 		$http({method: "JSONP", url: "http://thomasgerrald.esy.es/index.php", cache: $templateCache})
 		.success(function(data, status) {
 			$scope.names = data;
