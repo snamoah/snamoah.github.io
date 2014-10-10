2c5a003cab0a4c1f0fd9f9036156d9030733a800
Samuel Amoah <snamoah@gmail.com>
2014-10-10 13:20:08 +0000

    Connecting diary to server

--- diary/public/js/app.js
+++ diary/public/js/app.js
@@ -1,1 +1,7 @@
-	
+angular.module('notebookApp', [])
+	.controller('mainController', ['$scope', '$http', function($scope, $http) {
+		$http.get("http://thomasgerrald.esy.es/index.php")
+		.success( function(response) {
+			$scope.names = response;
+		});
+	}]);
