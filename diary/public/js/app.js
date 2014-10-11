d652f6e3bd83dd6dcc18d817169683e478788ea7
Samuel Amoah <snamoah@gmail.com>
2014-10-11 12:00:37 +0000

    Connect to server v5

--- diary/public/js/app.js
+++ diary/public/js/app.js
@@ -2,14 +2,15 @@ angular.module('notebookApp', [])
 	.controller('mainController', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
 		$scope.names = "This is me";
 		$scope.status = "";
-		$http({method: "JSONP", url: "http://thomasgerrald.esy.es/index.php", cache: $templateCache})
-		.success(function (data, status) {
-			$scope.names = data;
-			$scope.status = status;
-		})
-		.error(function (data, status) {
-			// body...
-			$scope.names = "Could not get data";
-			$scope.status = status;
+		$http.post("http://thomasgerrald.esy.es/test.php", {test: "test"})
+		//$http.post({method: "JSONP", url: "http://thomasgerrald.esy.es/index.php", cache: $templateCache})
+		.success(function (response) {
+			$scope.names = response;
+			//$scope.status = status;
 		});
+		// .error(function (data, status) {
+		// 	// body...
+		// 	$scope.names = "Could not get data";
+		// 	$scope.status = status;
+		// });
 	}]);
