024223e1643eea6fdc1d0a9dfa414b8d65a71215
Samuel Amoah <snamoah@gmail.com>
2014-10-14 01:03:13 +0000

    Connected diary to backend server in order to save text

--- diary/public/js/app.js
+++ diary/public/js/app.js
@@ -1,11 +1,15 @@
 angular.module('notebookApp', [])
 	.controller('mainController', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
-		$scope.names = "This is me";
+		$scope.names = {name: "test", text: "test"};
 		$scope.status = "";
-		$http.post("http://thomasgerrald.esy.es/test.php", {test: "test"})
+
+		var url = "http://thomasgerrald.esy.es/test.php";
+		$http.get(url+"?first_load=JSON_CALLBACK")
 		//$http.post({method: "JSONP", url: "http://thomasgerrald.esy.es/index.php", cache: $templateCache})
-		.success(function (response) {
-			$scope.names = response;
+		.success(function (data) {
+			$scope.names = data;
+			$scope.noteId = data['id'];
+			$scope.content = data['text'];
 			//$scope.status = status;
 		});
 		// .error(function (data, status) {
@@ -13,4 +17,29 @@ angular.module('notebookApp', [])
 		// 	$scope.names = "Could not get data";
 		// 	$scope.status = status;
 		// });
+
+		$scope.createNotebook =  function() {
+			$http.get(url+"?create=JSON_CALLBACK")
+			.success(function (data) {
+				$scope.names = data;
+			});
+		};
+
+		$scope.saveNotes = function() {
+			$http({
+				url: "http://thomasgerrald.esy.es/save.php",
+				method: "POST",
+				data: {id: $scope.noteId, text: $scope.content},
+				transformRequest: function(obj) {
+			        var str = [];
+			        for(var p in obj)
+			        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+			        return str.join("&");
+			    },
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).
+			success(function(data) {
+				console.log("It sent it: " + data);
+			});
+		};
 	}]);
