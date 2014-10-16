c746bf5b6f81f24558d5512c8f4f3fbea2cbeede
Samuel Amoah <snamoah@gmail.com>
2014-10-16 11:37:47 +0000

    names removed v2

--- rsg/public/js/controllers/randomStudentController.js
+++ rsg/public/js/controllers/randomStudentController.js
@@ -5,7 +5,7 @@ function randomStudentController($scope) {
 		//{name: "Blessing Onomesino", path: $scope.path +"blessing.jpg"}, 	
 		//{name: "Esther Olatunde", path: $scope.path +"esther.jpg"}, 
 		//{name: "Babara Okoto", path: $scope.path +"babara.jpg"},
-		{name: "Priscilla Hazel", path: $scope.path +"priscilla.jpg"},
+		//{name: "Priscilla Hazel", path: $scope.path +"priscilla.jpg"},
 		{name: "Prince Anim", path: $scope.path+"prince.jpg"},
 		{name: "David Evhade", path: $scope.path +"david.jpg"},
 		//{name: "Kennedy Anyinatoe", path: $scope.path +"kennedy.jpg"},
@@ -13,7 +13,7 @@ function randomStudentController($scope) {
 		//{name: "Innocent Udeogu", path: $scope.path +"innocent.jpg"},
 		//{name: "Philip Nunoo", path: $scope.path +"philip.jpg"},
 		//{name: "Cassandra Sarfo", path: $scope.path +"cassandra.jpg"},
-		{name: "Derek Frimpong", path: $scope.path +"derek.jpg"},
+		//{name: "Derek Frimpong", path: $scope.path +"derek.jpg"},
 		//{name: "Panin Tenkorang", path: $scope.path +"panin.jpg"},
 		//{name: "Paul Damalie", path: $scope.path +"paul.jpg"},
 		//{name: "Barnabas Ayinloya", path: $scope.path +"barnabas.jpg"},
@@ -25,10 +25,10 @@ function randomStudentController($scope) {
 		//{name: "Kelechi Uduagu", path: $scope.path +"kelechi.JPG"},
 		{name: "Afi Kwao", path: $scope.path +"afi.jpg"},
 		//{name: "Moses Abubakari", path: $scope.path +"moses.jpg"},
-		{name: "Benjamin Kissi", path: $scope.path +"ben.jpg"},
+		//{name: "Benjamin Kissi", path: $scope.path +"ben.jpg"},
 		//{name: "Abideen Adeleye", path: $scope.path +"abideen.jpg"},
 		{name: "Alexander Okereke", path: $scope.path +"alex.jpg"},
-		{name: "Samuel Nkoom Amoah", path: $scope.path +"samuel.jpg"}];
+		//{name: "Samuel Nkoom Amoah", path: $scope.path +"samuel.jpg"}];
 
 	$scope.randomStudents = [" ", " ", " "];
 
