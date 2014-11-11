d958b82977f363a3b0b28b05ef5208f696ed354d
Samuel Amoah <snamoah@gmail.com>
2014-11-11 12:56:29 +0000

    Repopulated students random generator

--- teamDivider/public/js/app.js
+++ teamDivider/public/js/app.js
@@ -35,7 +35,8 @@ angular.module('teamDividerApp', [])
 		$scope.indexes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
 
 
-		
+    $scope.show = false;
+
 		$scope.generateTable = function(number) {
 			var numberOfTeams = Math.floor($scope.students.length / number);
 
@@ -57,6 +58,7 @@ angular.module('teamDividerApp', [])
 			};
 
 			$scope.names = $scope.teams;
+      $scope.show = true;
 		};
 
 		$scope.displayIndexes = function() {
